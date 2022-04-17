f228fe74030dc20a4e90295827938ae783450dbe
Ajeet <ajeets.shah@hotmail.com>
2022-04-17 15:55:40 +0530

    Move index

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Hello, World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/greeting/GreetingController.java
+++ src/main/java/com/example/demo/greeting/GreetingController.java
@@ -12,11 +12,6 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping
-    public String index() {
-        return "Hello, World!";
-    }
-
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
